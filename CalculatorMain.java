6d9533a669cb14d5a70bfc1e68e683d521df3bc0
costaroot <dmitrijkosta92@gmail.com>
2020-05-25 16:17:36 +0300

    change basic message!

--- CalculatorMain.java
+++ CalculatorMain.java
@@ -9,7 +9,7 @@ public class CalculatorMain {
 
         while (true) {
             Scanner scan = new Scanner(System.in);
-            System.out.println("Please enter Rome or Arabic number in formats (Num1 operation Num2) and press 'Enter'");
+            System.out.println("Please enter Rome or Arabic number in formats (Num1 operation Num2) and press key 'Enter'");
             String inputLine = new String(scan.nextLine());
             inputLine = inputLine.trim();
 
